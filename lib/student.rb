6ce6baf9f99c8466f5201ac7dbf9d9cacc455227
Jason Vance <jbvance@gmail.com>
2017-12-01 14:10:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   def initialize(student_hash)
     self.name = student_hash[:name]
     self.location = student_hash[:location]
-    self.all << self
+    self.class.all << self
   end
 
   def self.create_from_collection(students_array)
